ec40dd9e5632e4cce0545a9e9324ad45f1eadd4a
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2022-11-21 23:43:14 +0530

    database update

--- Database/nenasa_nvestment_db (2).sql
+++ Database/nenasa_nvestment_db (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2022 at 08:32 AM
+-- Generation Time: Nov 21, 2022 at 07:12 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -44,7 +44,52 @@ CREATE TABLE `apply` (
 
 INSERT INTO `apply` (`apply_id`, `customer_id`, `job_id`, `resume`, `additional_details`, `is_deleted`, `date_updated`, `apply_status`) VALUES
 (1, 3, 1, 'testimonial-1.jpg', '<p>asasa</p>', 0, '2022-10-29 00:15:25', 0),
-(2, 3, 3, 'Project Report.pdf', '<p>sasas</p>', 0, '2022-10-29 00:43:36', 0);
+(2, 3, 3, 'Project Report.pdf', '<p>sasas</p>', 0, '2022-10-29 00:43:36', 0),
+(3, 3, 7, 'Project Report.pdf', '<p>sas</p>', 0, '2022-11-08 23:40:37', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `cat_image` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `is_deleted` int(2) NOT NULL,
+  `date_updated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_name`, `cat_image`, `is_deleted`, `date_updated`) VALUES
+(1, 'Shoe', 'download.jfif', 1, '2022-09-11 11:19:40'),
+(2, 'Slipers', 'images.jfif', 1, '2022-09-05 21:19:24'),
+(3, 'Slipers', 'images.jfif', 1, '2022-09-05 21:22:19'),
+(4, 'dd', 'images.jfif', 1, '2022-09-05 21:21:35'),
+(5, 'Phone Acc', 'hImYqMss.png', 1, '2022-09-05 22:23:22'),
+(6, '', 'bottles-famous-global-beer-brands-poznan-pol-mar-including-heineken-becks-bud-miller-corona-stella-artois-san-miguel-143170440.jpg', 1, '2022-10-01 10:17:33'),
+(7, 'Foods', 'bottles-famous-global-beer-brands-poznan-pol-mar-including-heineken-becks-bud-miller-corona-stella-artois-san-miguel-143170440.jpg', 0, '2022-10-01 10:18:50'),
+(8, 'Drinks', 'bottles-famous-global-beer-brands-poznan-pol-mar-including-heineken-becks-bud-miller-corona-stella-artois-san-miguel-143170440.jpg', 0, '2022-10-01 10:20:19'),
+(9, 'Burger', 'download.jfif', 0, '2022-10-01 14:49:14'),
+(10, 'Burger', 'download.jfif', 1, '2022-10-01 14:49:15'),
+(11, 'Burger', 'fast-food.jpg', 1, '2022-10-01 14:49:40'),
+(12, 'Tacos', 'aussie-style-beef-and-salad-tacos-86525-1.jpeg', 1, '2022-10-01 15:35:24'),
+(13, 'Dew', 'dsd.PNG', 1, '2022-10-01 22:24:45'),
+(14, 'Dews', 'dsd.PNG', 1, '2022-10-01 22:26:14'),
+(15, 'Main', 'download.jfif', 1, '2022-10-02 13:43:02'),
+(16, 'dew', 'download.jfif', 1, '2022-10-02 13:43:56'),
+(17, 'dew', 'download.jfif', 1, '2022-10-02 13:44:36'),
+(18, 'dew', 'download.jfif', 1, '2022-10-02 13:46:58'),
+(19, 'dew', 'Basin Mixer.jpg', 1, '2022-10-02 13:48:13'),
+(20, 'Beer', 'images.jfif', 1, '2022-10-02 15:37:27'),
+(21, 'Beer', 'images.jfif', 1, '2022-10-02 15:37:29'),
+(22, 'Beer', 'images.jfif', 1, '2022-10-02 15:37:48'),
+(23, 'Beer', 'images.jfif', 1, '2022-10-02 15:37:55'),
+(24, 'ss', 'download.jpg', 0, '2022-11-20 08:36:49');
 
 -- --------------------------------------------------------
 
@@ -76,8 +121,11 @@ CREATE TABLE `company` (
 --
 
 INSERT INTO `company` (`company_id`, `company_name`, `tagline`, `company_description`, `website`, `facbook`, `twitter`, `lonkdin`, `company_login_email`, `company_password`, `company_admin_email`, `company_admin_phone`, `company_logo`, `is_deleted`, `date_updated`, `permision`) VALUES
-(1, 'SEUSL', 'sas', '<p>sas</p>', 'sasa', 'sas', 'sas', 'sasa', '', '123456', 'kanishkadewsandaruwan@gmail.com', 2147483647, 'product-1.jpg', 0, '2022-10-28 17:14:51', 1),
-(2, 'Maytech Technology SL', 'Maytech', '<p>aasas dsdsds sasa</p>', '', '', '', '', 'company@gmail.com', '12345', 'kanishkadewsandaruwan@gmail.com', 713664071, 'job_logo_5.jpg', 0, '2022-10-28 17:28:48', 1);
+(1, 'SEUSL', 'sas', '<p>sas</p>', 'sasa', 'sas', 'sas', 'sasa', 'kanishkadewsn@gmail.com', '123456', 'kanishkadewsn@gmail.com', 2147483647, 'product-1.jpg', 1, '2022-10-28 17:14:51', 1),
+(2, 'Maytech Technology SL', 'Maytech', '<p>aasas dsdsds sasa</p>', '', '', '', '', 'company@gmail.com', '12345', 'kanishkadewsandaruwan@gmail.com', 713664071, 'job_logo_5.jpg', 1, '2022-10-28 17:28:48', 1),
+(3, 'Maytech Technology', 'sas', '<p>ss</p>', 'sa', 'sas', 'sas', 'sas', 'maytech@gmail.com', '123456', 'maytech@gmail.com', 713664071, 'banner-2.jpg', 0, '2022-11-08 00:41:04', 1),
+(4, 'ss', 'ss', '<p>sss</p>', 'ss', 'sas', 'sas', 'sas', 'admin@gmail.com', '123456', 'kanishkadewsandaruwan@gmail.com', 723694071, 'Basin Mixer.jpg', 0, '2022-11-09 00:16:33', 0),
+(5, 'Maytech Technology SLS', 'sas', '<p>sasas</p>', 'ss', 'ss', 'ss', 'ss', 'dew@gmail.com', '123456', 'kanishkadewsandaruwan@gmail.com', 713664071, 'download.jpg', 1, '2022-11-20 11:25:52', 1);
 
 -- --------------------------------------------------------
 
@@ -101,7 +149,7 @@ CREATE TABLE `contact` (
 INSERT INTO `contact` (`contact_id`, `name`, `email`, `subject`, `message`, `date_updated`) VALUES
 (4, 'Kanishka Dew Sandaruwan', 'kanishkadewsandaruwan@gmail.com', '0713664071', 'sas', '2022-09-12 22:35:23'),
 (5, 'Kanishka Dew Sandaruwan', 'kanishkadewsandaruwan@gmail.com', '0713664071', 'dsds', '2022-09-15 10:28:09'),
-(14, 'Kanishka Dew Sandaruwan', 'kanishkadewsandaruwan@gmail.com', 'ss', 'ss', '2022-10-02 11:47:28');
+(15, 'Muthuwana Acharige Kanishka Dew Sandaruwan', 'kanishkadewsandaruwan@gmail.com', 'sss', 'sss', '2022-11-07 13:50:56');
 
 -- --------------------------------------------------------
 
@@ -130,7 +178,9 @@ INSERT INTO `customer` (`customer_id`, `name`, `email`, `phone`, `nic`, `address
 (1, '', 'admin', '', '', '', 0, '123456', 0, 1),
 (2, 'Thilini Maheshika', 'thili@gmail.com', '0713664071', '962670426G', 'Banwalgodalla, Kosmulla', 2, '123456', 1, 2),
 (3, 'Kanishka Dew Sandaruwan', 'kanishkadewsandaruwan@gmail.com', '0713664071', '962670426G', 'Banwalgodalla, Kosmulla', 1, '123456', 0, 1),
-(4, 'Thilini Maheshika', 'thili@gmail.com', '0713664071', '992670426V', 'Banwalgodalla, Kosmulla', 0, '123456', 0, 2);
+(4, 'Thilini Maheshika', 'thili@gmail.com', '0713664071', '992670426V', 'Banwalgodalla, Kosmulla', 0, '123456', 1, 2),
+(5, 'Muthuwana Acharige Kanishka Dew Sandaruwan', 'thili@gmail.com', '0723694071', '992670426V', 'Banwalgodalla, Kosmulla', 1, '123456', 1, 2),
+(6, 'Muthuwana Acharige Kanishka Dew Sandaruwan', 'Kanishkadewsandaruwan9@gmail.com', '0723694071', '992670426V', 'Banwalgodalla, Kosmulla', 1, '123456', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -149,20 +199,23 @@ CREATE TABLE `job` (
   `job_active` int(2) NOT NULL,
   `closing_date` date NOT NULL,
   `date_updated` datetime NOT NULL,
-  `company_id` int(11) NOT NULL
+  `company_id` int(11) NOT NULL,
+  `cat_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `job`
 --
 
-INSERT INTO `job` (`job_id`, `job_image`, `job_title`, `job_location`, `type`, `job_description`, `is_deleted`, `job_active`, `closing_date`, `date_updated`, `company_id`) VALUES
-(1, 'img_1.jpg', 'Software Engineer ', 'Colombo', 0, '<p>asasasÂ </p>', 0, 1, '2022-10-31', '0000-00-00 00:00:00', 2),
-(2, 'testimonial-2.jpg', 'Marketing Manager', 'Colombo', 0, '<p>asasasa</p>', 0, 0, '2022-10-28', '0000-00-00 00:00:00', 2),
-(3, 'testimonial-3.jpg', 'Web developer', 'Colombo', 1, '<p>saaasa</p>', 0, 0, '2022-10-28', '0000-00-00 00:00:00', 2),
-(4, 'blog-1.jpg', 'Assosiate Software Engineer ', 'Colombo', 1, '<p>sasasas</p>', 0, 0, '2022-10-28', '0000-00-00 00:00:00', 2),
-(5, 'blog-2.jpg', 'Sineor Software Engineer', 'Matara', 0, '<p>asasasa</p>', 0, 0, '0000-00-00', '2022-10-28 22:42:39', 2),
-(6, 'about.jpg', 'Farmer', 'Matara', 0, '<p>ddsds</p>', 0, 0, '2022-11-01', '2022-10-28 22:44:20', 2);
+INSERT INTO `job` (`job_id`, `job_image`, `job_title`, `job_location`, `type`, `job_description`, `is_deleted`, `job_active`, `closing_date`, `date_updated`, `company_id`, `cat_id`) VALUES
+(1, 'img_1.jpg', 'Software Engineer ', 'Colombo', 0, '<p>asasasÂ </p>', 0, 1, '2022-10-31', '0000-00-00 00:00:00', 2, 0),
+(2, 'testimonial-2.jpg', 'Marketing Manager', 'Colombo', 0, '<p>asasasa</p>', 1, 0, '2022-10-28', '0000-00-00 00:00:00', 2, 0),
+(3, 'testimonial-3.jpg', 'Web developer', 'Colombo', 1, '<p>saaasa</p>', 0, 0, '2022-10-28', '0000-00-00 00:00:00', 2, 0),
+(4, 'blog-1.jpg', 'Assosiate Software Engineer ', 'Colombo', 1, '<p>sasasas</p>', 0, 0, '2022-10-28', '0000-00-00 00:00:00', 2, 0),
+(5, 'blog-2.jpg', 'Sineor Software Engineer', 'Matara', 0, '<p>asasasa</p>', 0, 0, '0000-00-00', '2022-10-28 22:42:39', 2, 0),
+(6, 'about.jpg', 'Farmer', 'Matara', 0, '<p>ddsds</p>', 0, 0, '2022-11-01', '2022-10-28 22:44:20', 2, 0),
+(7, 'Basin Mixer.jpg', 'Software ', 'Colombo', 0, '<p><b>Software Enginerr virtsa</b></p>', 0, 0, '2022-11-17', '2022-11-08 00:46:25', 3, 0),
+(8, 'images (1).jpeg', 'Software Engineer', 'Colombo', 0, '<p>sas</p>', 0, 0, '2022-11-25', '2022-11-20 08:41:55', 3, 9);
 
 -- --------------------------------------------------------
 
@@ -175,7 +228,6 @@ CREATE TABLE `review` (
   `review_review` varchar(255) NOT NULL,
   `review_name` varchar(255) NOT NULL,
   `review_email` varchar(255) NOT NULL,
-  `review_pid` int(11) NOT NULL,
   `date_updated` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -183,9 +235,10 @@ CREATE TABLE `review` (
 -- Dumping data for table `review`
 --
 
-INSERT INTO `review` (`review_id`, `review_review`, `review_name`, `review_email`, `review_pid`, `date_updated`) VALUES
-(2, 'sasa', 'Kanisha', 'kanishkadewsandaruwan@gmail.com', 16, '2022-10-21 16:19:15'),
-(3, 'asasas', 'Sandaruwan', 'asa@gmail.com', 16, '2022-10-21 16:31:48');
+INSERT INTO `review` (`review_id`, `review_review`, `review_name`, `review_email`, `date_updated`) VALUES
+(2, 'sasa', 'Kanisha', 'kanishkadewsandaruwan@gmail.com', '2022-10-21 16:19:15'),
+(3, 'asasas', 'Sandaruwan', 'asa@gmail.com', '2022-10-21 16:31:48'),
+(5, 'ss', 'Kanisha', 'ss', '2022-11-08 00:30:51');
 
 -- --------------------------------------------------------
 
@@ -214,7 +267,7 @@ CREATE TABLE `settings` (
 --
 
 INSERT INTO `settings` (`header_image`, `header_title`, `header_desc`, `about_title`, `about_desc`, `company_phone`, `company_email`, `company_address`, `sub_image`, `about_image`, `link_facebook`, `link_twiiter`, `link_instragram`) VALUES
-('hero_1.jpg', 'Welcome to Nanasa', 'With this shop hompeage ', 'About Us', 'sajskajskajskjask', '0713664076', 'asn@gmail.com', 'Banwalgodalla, Kosmulla', 'mcdonaldsglobal.jpg', 'img_1.jpg', 'https://www.facebook.com/', 'https://www.facebook.com/', 'https://www.facebook.com/');
+('hero_1.jpg', 'Welcome to Nanasa', 'With this shop hompeage ', 'About Us', 'sajskajskajskjask', '0713664076', 'asn@gmail.com', 'Banwalgodalla, Kosmulla', 'banner-1.jpg', 'img_1.jpg', 'https://www.facebook.com/', 'https://www.facebook.com/', 'https://www.facebook.com/');
 
 --
 -- Indexes for dumped tables
@@ -225,6 +278,12 @@ INSERT INTO `settings` (`header_image`, `header_title`, `header_desc`, `about_ti
 --
 ALTER TABLE `apply`
   ADD PRIMARY KEY (`apply_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
 
 --
 -- Indexes for table `company`
@@ -264,37 +323,43 @@ ALTER TABLE `review`
 -- AUTO_INCREMENT for table `apply`
 --
 ALTER TABLE `apply`
-  MODIFY `apply_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `apply_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
-  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `job`
 --
 ALTER TABLE `job`
-  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
